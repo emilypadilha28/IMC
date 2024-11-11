e2d84084b78fddbb782e47c467c89d396a11eae0
emily padilha <141257057+emilypadilha28@users.noreply.github.com>
2024-11-11 09:56:19 -0300

    Add files via upload

--- imc.js
+++ imc.js
@@ -1,3 +1,12 @@
-let peso = partseFoat (prompt("informe o seu peso"));
-let altura = parseFloat (promp("informe a sua altura"));
+let peso = parseFloat(prompt("informe o seu peso:"));
+let altura=parseFloat(prompt("informe sua altura"));
 
+if( isNaN(peso) || ispn(altura)||peso<=0||altura){
+document.write("porfavor informar valores validos!!!");}
+else{
+let imc=(peso/altura*altura)
+document.write("o seu imc é igual a;")
+if(imc>=18 && imc <249){
+document.write("peso ideal")
+}else if (imc>249 && imc<=28){
+}}
